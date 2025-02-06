299d0212195c9d80158b3a3bf1c738bc1ea856f9
MAKANESHA <127739640+Makanesha@users.noreply.github.com>
2025-02-06 14:25:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u makanesha -p 041204nesha
-    docker tag test makanesha/guvi:sample
-    docker push makanesha/guvi:sample
-    
+
+echo hi123
+
+# Docker build and push commands
+sh 'chmod +x build.sh'
+sh './build.sh'
+docker login -u makanesha -p 041204nesha
+docker build -t test .
+docker tag test makanesha/guvi:sample
+docker push makanesha/guvi:sample
+docker run -itd -p 93:80 makanesha/guvi:sample
+
+# Kubernetes commands to create deployment, expose it, and get service details
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl get service test
