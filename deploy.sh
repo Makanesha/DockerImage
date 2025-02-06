2c9da088f018aea0354c8007854d6265f5f4ae63
MAKANESHA <127739640+Makanesha@users.noreply.github.com>
2025-02-06 16:00:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ docker login -u makanesha -p 041204nesha
 docker build -t test .
 docker tag test makanesha/guvi:sample
 docker push makanesha/guvi:sample
-docker run -itd -p 94:80 makanesha/guvi:sample
+
 
 # Kubernetes commands to create deployment, expose it, and get service details
