d7e83c92e9694ec1caeafc86c31652d6ad43d8e8
MAKANESHA <127739640+Makanesha@users.noreply.github.com>
2025-02-08 09:17:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
-
-# Output message to confirm script execution
 echo "Starting the Docker build and push process..."
-
-# Docker login
 docker login -u makanesha -p 041204nesha
-
-# Build the Docker image
 docker build -t test .
-
-# Tag the Docker image
 docker tag test makanesha/guvi:sample
-
-# Push the Docker image to Docker Hub
 docker push makanesha/guvi:sample
-
 echo "Docker image pushed successfully!"
 
-# Now run the Kubernetes deployment and expose the service
-
-# Create the deployment using kubectl
 kubectl apply -f deployment.yaml
-
-# Create the service using kubectl
 kubectl apply -f service.yaml
-
-# Get the service details to confirm everything is running correctly
 kubectl get svc test
-
-# Optionally, you can check the pods as well
 kubectl get pods
